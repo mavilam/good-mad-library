f2015646299a8632c1ba399588154f620f5502f6
Miguel Ávila Monroy <soyer893@gmail.com>
2020-06-09 17:31:49 +0200

    Permitir peticiones de varios usuarios

--- src/init.ts
+++ src/init.ts
@@ -16,14 +16,16 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
-var userId : string = ""
 const siteURL : string = process.env['URL']
+const sitePort : Number = Number(process.env['PORT'])
 
 app.get('/', function(req, res) {
   gr.initOAuth(`${siteURL}/goodreads_oauth_callback`)
   return gr.getRequestToken()
-    .then(url => { 
-      return res.redirect(url)
+    .then(url => {
+      const htmlbase : string = readHtml('gotoread.html')
+      writeHtml(htmlbase.replace('<!--link-->', url), 'gotoread.html')
+      return res.sendFile(path.join(__dirname, 'gotoread.html'))
     })
     .catch(err => {
       console.log(err)
@@ -36,9 +38,9 @@ app.get('/goodreads_oauth_callback', (req, res) => {
   .then(() => {
     gr.getCurrentUserInfo()
     .then(userInfo => { 
-      userId = userInfo.user.id
+      const userId = userInfo.user.id
       console.log(`${userId} - Callback`)
-      return res.redirect('/to-read')
+      return res.redirect(`/to-read?userid=${userId}`)
     })
     .catch(err => {
       console.log(err)
@@ -49,13 +51,14 @@ app.get('/goodreads_oauth_callback', (req, res) => {
 
 app.get('/to-read', async (req, res) => {
   try{
-      let bookArr : string[] = await getToReadBooks(gr, userId)
-      if (!bookArr || bookArr.length < 1) return res.status(200).sendFile(path.join(__dirname, '../front/noBooks.html'))
-      const bookData : BookData[] = bookArr.map(book => new BookData(book))
-      const htmlbase : string = readHtml()
-      const bookListDivs : string = composeHtml(bookData)
-      writeHtml(htmlbase.replace('<!--body-->', bookListDivs))
-      return res.sendFile(path.join(__dirname, 'index.html'))
+    const userId = req.query.userid
+    let bookArr : string[] = await getToReadBooks(gr, userId)
+    if (!bookArr || bookArr.length < 1) return res.status(200).sendFile(path.join(__dirname, '../front/noBooks.html'))
+    const bookData : BookData[] = bookArr.map(book => new BookData(book))
+    const htmlbase : string = readHtml('index.html')
+    const bookListDivs : string = composeHtml(bookData)
+    writeHtml(htmlbase.replace('<!--body-->', bookListDivs), `index-${userId}.html`)
+    return res.sendFile(path.join(__dirname, `index-${userId}.html`))
   } catch(err) {
     console.log(err)
     if (err.message.includes('need an oAuth')) return res.redirect(siteURL)
@@ -63,6 +66,6 @@ app.get('/to-read', async (req, res) => {
   }
 })
 
-app.listen(Number(process.env['PORT']), function () {
-  console.log("I'm running")
+app.listen(sitePort, function () {
+  console.log(`I'm running on ${siteURL}:${sitePort}`)
 })
--- src/utils/htmlUtils.ts
+++ src/utils/htmlUtils.ts
@@ -10,12 +10,12 @@ function composeBookElement(book) : string {
   return `<div class=\"book\"><img src=\"${book.image}\" alt=\"Book cover\"><p>${book.title} de ${book.author}</p><a href=\"${book.libraryLinks.paper}\">Biblioteca</a><br><a href=\"${book.libraryLinks.ebiblio}\">eBiblio</a></div>`
 }
 
-function readHtml() : string {
-  return fs.readFileSync('/app/front/_index.html', 'utf-8')
+function readHtml(name) : string {
+  return fs.readFileSync(path.join(__dirname, `../../front/_${name}`), 'utf-8')
 }
 
-function writeHtml(html) : void {
-  fs.writeFileSync(path.join(__dirname, '../index.html'), html, {
+function writeHtml(html, name) : void {
+  fs.writeFileSync(path.join(__dirname, `../${name}`), html, {
     flag: 'w'
   })
 }
