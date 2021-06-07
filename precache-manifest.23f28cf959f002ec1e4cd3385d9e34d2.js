a327babf572ab193349f44ca2dafce4290d2ebeb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-07 07:38:47 +0000

    Deploy mavilam/good-mad-library to github.com/mavilam/good-mad-library.git:gh-pages

---
+++ precache-manifest.23f28cf959f002ec1e4cd3385d9e34d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71957533b2ce75ae368e96d67f542a8",
+    "url": "/good-mad-library/android-chrome-192x192.png"
+  },
+  {
+    "revision": "6202cabed92989a16fdde76dfd6df481",
+    "url": "/good-mad-library/android-chrome-512x512.png"
+  },
+  {
+    "revision": "0102214cca60a3f1364df439f1ecfe83",
+    "url": "/good-mad-library/apple-touch-icon.png"
+  },
+  {
+    "revision": "f5101475a4ff0362ae513745916b8249",
+    "url": "/good-mad-library/favicon-16x16.png"
+  },
+  {
+    "revision": "ec76fac6cdd24312af647bcc7376c19f",
+    "url": "/good-mad-library/favicon-32x32.png"
+  },
+  {
+    "revision": "b40a187c688bca115fe7025497db998d",
+    "url": "/good-mad-library/index.html"
+  },
+  {
+    "revision": "7a4d4ab4ed3de9f68211cb5fc1a3041d",
+    "url": "/good-mad-library/manifest.json"
+  },
+  {
+    "revision": "1b2002e42c362c66f19c",
+    "url": "/good-mad-library/static/css/app.98c7d3f5.css"
+  },
+  {
+    "revision": "6b6ab2fd87af91249d17",
+    "url": "/good-mad-library/static/css/chunk-vendors.cf94ea8f.css"
+  },
+  {
+    "revision": "1b2002e42c362c66f19c",
+    "url": "/good-mad-library/static/js/app.bf473336.js"
+  },
+  {
+    "revision": "6b6ab2fd87af91249d17",
+    "url": "/good-mad-library/static/js/chunk-vendors.7617683f.js"
+  }
+]);
